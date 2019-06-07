8e1f952a51367656c2feeaa03bd898ca798fc6c1
Ido <idomarko1998@gmail.com>
2019-06-07 13:30:59 +0300

    Picklable_Priorty_Queue

---
+++ priority_queue_picklable.py
@@ -0,0 +1,36 @@
+from multiprocessing import Queue
+
+
+class Picklable_Priorty_Queue:
+
+    def __init__(self):
+        queue = Queue()
+
+    def put(self, state):
+        temp_queue = Queue()
+        inserted = False
+        #while loop to get the new item in the temp queue
+        while (not self.queue.empty()):
+            temp_state = self.queue.get()
+            if(state > temp_state): #Not sure what should be here
+                temp_queue.put(state)
+                temp_queue.put(temp_state)
+                inserted = True
+                break
+            else:
+                temp_queue.put(temp_state)
+
+        #if the new state is the smallest, then it wouldn't been added yet
+        if(not inserted):
+            temp_queue.put(state)
+
+        #while loop to move all item left from original queue to temp queue
+        while(not self.queue.empty()):
+            temp_queue.put(self.queue.get())
+
+        #move all items back to the original queue
+        while(not temp_queue.empty()):
+            self.queue.put(temp_queue.get())
+
+    def get(self):
+        return self.queue.get()
